f5f9aac410f8fc7858ab937f4f122a65030ab597
EVR005 <edumbasankaravel513@gmail.com>
2023-06-04 15:09:31 +0530

    prevented reload

--- src/Components/products.js
+++ src/Components/products.js
@@ -27,11 +27,7 @@ export default function Products(props) {
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
-        <AddToCart
-          open={open}
-          details={Object.assign({}, details)}
-          setOpen={setOpen}
-        ></AddToCart>
+        <AddToCart open={open} details={details} setOpen={setOpen}></AddToCart>
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {props.products.map((product) => {
             return (
